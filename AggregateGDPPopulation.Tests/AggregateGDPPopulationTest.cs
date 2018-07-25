fe590356266ce4a126693fc40e7e84d70e06c680
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-25 11:22:01 +0530

    after review

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -12,9 +12,10 @@ namespace AggregateGDPPopulation.Tests
         [Fact]
        async public void Test1()
         {
-           await Class1.Calgdppopasync();
-            var actualoutput = Class1.reader("../../../../AggregateGDPPopulation/output/output.json");
-            var expectedoutput = Class1.reader("../../../expected-output.json");
+           await AggregateGDPPopulation.calAggregate();
+            Fileoperation fileobj1 = new Fileoperation();
+            var actualoutput = fileobj1.reader("../../../../AggregateGDPPopulation/output/output.json");
+            var expectedoutput = fileobj1.reader("../../../expected-output.json");
             string str1 = await actualoutput;
             string str2 = await expectedoutput;
             Assert.Equal(str1,str2);
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -6,68 +6,94 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 namespace AggregateGDPPopulation
 {
-    public class countrygdppop
+    public class Countrygdppopulation
     {
         public float GDP_2012;
         public float POPULATION_2012;
     }
-    public class Class1
+    public class Fileoperation
     {
-           async public static Task<string> reader(string filepath)
-           {
-                 StreamReader sr = new StreamReader(filepath);
-                 string line = await sr.ReadToEndAsync();
-                 return line;
-           }
-            async public static Task writer(string filepath,string obj)
+        async public Task<string> reader(string filepath)
+        {
+            StreamReader sr = new StreamReader(filepath);
+            string line = await sr.ReadToEndAsync();
+            return line;
+        }
+    
+        async public Task writer(string filepath, string obj)
+        {
+            using (StreamWriter sr = new StreamWriter(filepath))
             {
-                using (StreamWriter sr = new StreamWriter(filepath))
-                {
-                    await sr.WriteAsync(obj);
-                }  
+                await sr.WriteAsync(obj);
             }
-         async public static Task Calgdppopasync()
-           {
-            try
+        }
+    }
+    public class DeserializeobjectToDictionary
+    {
+        public Dictionary<string,string> deserialize(string str)
+        {
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
+            return values;
+        }
+    }
+    public class Aggregate
+    {
+        public string aggregateGDPPopulation(string data,Dictionary<string,string> values)
+        {
+            Dictionary<string, Countrygdppopulation> result = new Dictionary<string, Countrygdppopulation>();
+            string[] array = data.Split('\n');
+            array[0] = array[0].Replace("\"", "");
+            string[] headers = array[0].Split(',');
+            int countryname = Array.IndexOf(headers, "Country Name");
+            int gdpindex = Array.IndexOf(headers, "GDP Billions (USD) 2012");
+            int popindex = Array.IndexOf(headers, "Population (Millions) 2012");
+            foreach (string s in array)
             {
-                var data = reader("../../../../AggregateGDPPopulation/data/datafile.csv");
-                var mapper = reader("../../../../AggregateGDPPopulation/continent.json");
-                await Task.WhenAll(data, mapper);
-                Dictionary<string, countrygdppop> result = new Dictionary<string, countrygdppop>();
-                var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(mapper.Result);
-                string[] array = data.Result.Split('\n');
-                array[0] = array[0].Replace("\"", "");
-                string[] headers = array[0].Split(',');
-                int countryname = Array.IndexOf(headers, "Country Name");
-                int gdpindex = Array.IndexOf(headers, "GDP Billions (USD) 2012");
-                int popindex = Array.IndexOf(headers, "Population (Millions) 2012");
-                foreach (string s in array)
+                string[] str = s.Replace("\"", "").Split(',');
+                if (values.ContainsKey(str[countryname]) == true)
                 {
-                    string[] str = s.Replace("\"","").Split(',');
-                    if (values.ContainsKey(str[countryname]) == true)
+                    if (result.ContainsKey(values[str[countryname]]) == false)
                     {
-                        if (result.ContainsKey(values[str[countryname]]) == false)
-                        {
-                            countrygdppop cobj = new countrygdppop();
-                            result.Add(values[str[countryname]], cobj);
-                            result[values[str[countryname]]].GDP_2012 = float.Parse(str[gdpindex]);
-                            result[values[str[countryname]]].POPULATION_2012 = float.Parse(str[popindex]);
-                           
-                        }
-                        else
-                        {
-                            result[values[str[countryname]]].GDP_2012 += float.Parse(str[gdpindex]);
-                            result[values[str[countryname]]].POPULATION_2012 += float.Parse(str[popindex]);
-                        }
+                        Countrygdppopulation cobj = new Countrygdppopulation();
+                        result.Add(values[str[countryname]], cobj);
+                        result[values[str[countryname]]].GDP_2012 = float.Parse(str[gdpindex]);
+                        result[values[str[countryname]]].POPULATION_2012 = float.Parse(str[popindex]);
+
+                    }
+                    else
+                    {
+                        result[values[str[countryname]]].GDP_2012 += float.Parse(str[gdpindex]);
+                        result[values[str[countryname]]].POPULATION_2012 += float.Parse(str[popindex]);
                     }
                 }
-                string json = JsonConvert.SerializeObject(result, Formatting.Indented); 
-               await writer("../../../../AggregateGDPPopulation/output/output.json", json);
             }
-            catch(Exception e)
-            {
-                Console.WriteLine(e);
-            }
-         }
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            return json;
+        }
+
     }
+
+
+    public class AggregateGDPPopulation
+    {
+        async public static Task calAggregate()
+        {
+            Fileoperation fileobj = new Fileoperation();
+            var data =await fileobj.reader("../../../../AggregateGDPPopulation/data/datafile.csv");
+            var mapper = await fileobj.reader("../../../../AggregateGDPPopulation/continent.json");
+            DeserializeobjectToDictionary dobj = new DeserializeobjectToDictionary();
+            var values = dobj.deserialize(mapper);
+            Aggregate aggregateobj = new Aggregate();
+            string str = aggregateobj.aggregateGDPPopulation(data, values);
+            await fileobj.writer("../../../../AggregateGDPPopulation/output/output.json", str);
+        }
+    }
+
+
+
+
+
+
+
+    
 }
